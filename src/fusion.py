94f60c473b1713774976a91ea64ccadfa497da48
gregoriiv <64691317+gregoriiv@users.noreply.github.com>
2021-12-13 12:54:05 +0100

    fusion operations update

--- src/fusion.py
+++ src/fusion.py
@@ -3,16 +3,25 @@ import sys
 import geopandas as gpd
 import pandas as pd
 import numpy as np
+from scipy.spatial.distance import cdist
+from scipy.spatial import cKDTree
+from shapely import geometry
 from collection import gdf_conversion
 from db.db import Database
 from db.config import DATABASE
-#from sqlalchemy import create_engine
 
 # Create connection to geonode db
 def geonode_connection():
     db = Database()
     con = db.connect()
     return con
+
+# Publish dataframe in geonode datastore 
+# df - dataframe, name - table name to store, if_exists="replace" to overwrite datatable
+def df2geonode(df,name,if_exists='replace'):
+    db = Database()
+    con = db.connect_engine()
+    df.to_postgis(con=con, name=name, if_exists=if_exists)
 
 # Returns geonode table as a dataframe 
 def geonode_table2df(con, table_name, geometry_column='geometry'):
@@ -21,8 +30,8 @@ def geonode_table2df(con, table_name, geometry_column='geometry'):
     return df
 
 # Returns study area as df from geonode db (germany_municipalities) according to rs code 
-def study_area2df(con,rs):
-    query = "SELECT * FROM germany_municipalities WHERE rs = '%s'" % rs
+def study_area2df(con,sdv_rs):
+    query = "SELECT * FROM germany_municipalities WHERE sdv_rs = '%s'" % sdv_rs
     df_area = gpd.read_postgis(con=con,sql=query, geom_col='geom')
     df_area = df_area.filter(['geom'], axis=1)
     return df_area
@@ -66,68 +75,173 @@ def addr_deaggregate(addr_street):
        
     return street, number
 
-def base2area(df_base,df_area):
-    df_base2area = gpd.overlay(df_base, df_area, how='intersection')
-    return df_base2area
+def df2area(df,df_area):
+    df2area = gpd.overlay(df, df_area, how='intersection')
+    return df2area
+
+def find_nearest(gdA, gdB, max_dist):
+    gdA.crs = "epsg:4326"
+    gdB.crs = "epsg:4326"
+    gdA = gdA.to_crs(31468)
+    gdB = gdB.to_crs(31468)
+    nA = np.array(list(gdA.geometry.apply(lambda x: (x.x, x.y))))
+    nB = np.array(list(gdB.geometry.apply(lambda x: (x.x, x.y))))
+    btree = cKDTree(nB)
+    dist, idx = btree.query(nA, k=1)
+    # gdB_nearest = gdB.iloc[idx].drop(columns={"geometry"}).reset_index(drop=True)
+    gdB_nearest = gdB.iloc[idx].reset_index(drop=True)
+    gdA = gdA.rename(columns={"addr:street": "street"})
+    gdf = pd.concat(
+        [
+            gdA.reset_index(drop=True),
+            gdB_nearest["osm_id"],
+            pd.Series(dist, name='dist')
+        ], 
+        axis=1)
+
+    gdf = gdf.rename(columns={"street": "addr:street", "city": "addr:city", "postcode" : "addr:postcode", "country": "addr:country"})
+    gdf_fus = gdf[gdf.dist < max_dist]
+    m = ~gdf.id.isin(gdf_fus.id)
+    gdf_not_fus = gdf[m]
+    gdf_fus = gdf_fus.to_crs(4326)
+    gdf_not_fus = gdf_not_fus.to_crs(4326)
+    gdf_fus = gdf_fus.drop(columns={"dist"})
+    gdf_not_fus = gdf_not_fus.drop(columns={"dist"})
+
+    return gdf_fus, gdf_not_fus
 
 # POIs fusion function for fusion custom pois data with osm data, referenced to table "germany_municipalities" with regions 
 # RETURNS tuple(df,name) and saves data as file if is specified
-# - con - sqlalchemy con with connection to geonode database
-# - table_input - df with data to fuse with osm 
-# - table_base - df of base osm data
-# - df_area - df with polygon of study area 
+# - df_base2area - df with base data  (osm) 
+# - df_area - df with polygon of study area
+# - df_input - df with data to fuse
 # - amenity_replace - str name of amenity which objects will be removed from base osm df
 # - amenity_update - str name of amenity to update (works together with brand_replace)
 # - brand_replace - str brand to replace (works)
 # - columns2drop_input - list(str) with column names which are not merging to base os df 
 # - return_name - str name of data, can be used as filename
 # - return_type - str ("GeoJSON", "GPKG") file type for storage return
-
-
-def replace_data_area(df_base2area, df_area, df_input, osm_location = False , amenity_replace=None, amenity_brand_replace=None, columns2rename=None, columns2drop=None, return_name = None, return_type=None):
+def replace_data_area(df_base2area, df_area, df_input, amenity_replace=None, amenity_set=False, amenity_brand_replace=None, 
+                      columns2rename=None, columns2drop=None, column_set_value=None, return_name = None, return_type=None):
     # Cut data to given area 
     df_input2area = gpd.overlay(df_input, df_area, how='intersection')
 
     # Remove amenity class from base dataframe
     if amenity_replace:
         df_base2area = df_base2area[df_base2area.amenity != amenity_replace]
-        if osm_location:
-            df_base_amenity = df_base2area[df_base2area.amenity == amenity_replace]
     elif amenity_brand_replace:
         amenity_brand_replace = eval(amenity_brand_replace)
-        df_base2area = df_base2area[(df_base2area.amenity != amenity_brand_replace[0]) & (df_base2area.brand != amenity_brand_replace[1])]
-        if osm_location:
-            df_base_amenity_brand = df_base2area[(df_base2area.amenity == amenity_brand_replace[0]) & (df_base2area.brand == amenity_brand_replace[1])]
+        df_base2area = df_base2area[(df_base2area.operator.str.lower() != amenity_brand_replace[1].lower()) & (df_base2area.amenity.str.lower() != amenity_brand_replace[0].lower())]
     else:
         print("Amenity (and brand) were not specified.. ")
 
     # Prepare input data for concatination
-    df_input2area = df_input2area.rename(columns=columns2rename)
-    df_input2area = df_input2area.drop(columns={*columns2drop})
+    if columns2rename:
+        df_input2area = df_input2area.rename(columns=columns2rename)
+    if columns2drop:
+        df_input2area = df_input2area.drop(columns={*columns2drop})
+
     if amenity_brand_replace:
         df_input2area['amenity'] = amenity_brand_replace[0]
         df_input2area['brand'] = amenity_brand_replace[1]
+        df_input2area['operator'] = amenity_brand_replace[1].lower()
+    elif amenity_set:
+        df_input2area['amenity'] = amenity_replace
+
     df_input2area['amenity'] = df_input2area['amenity'].str.lower()
 
     # Set tags and geom from osm data to input
 
     # Deaggregate street and number
-    for i in df_input2area.index:
-        df_row = df_input2area.iloc[i]
-        address = addr_deaggregate(df_row["addr:street"])
-        df_input2area.at[i,"addr:street"] = address[0]
-        df_input2area.at[i,"housenumber"] = address[1]
+    if 'housenumber' not in df_input2area.columns:
+        for i in df_input2area.index:
+            df_row = df_input2area.iloc[i]
+            address = addr_deaggregate(df_row["addr:street"])
+            df_input2area.at[i,"addr:street"] = address[0]
+            df_input2area.at[i,"housenumber"] = address[1]
+
 
     # Concatination of dataframes
     df_result = pd.concat([df_base2area,df_input2area],sort=False).reset_index(drop=True)
     df_result = df_result.replace({np.nan: None})
 
+    if column_set_value:
+        for col in column_set_value.keys():
+            df_result[col] = column_set_value[col]
+
     # Writedown result of fusion
     return gdf_conversion(df_result, return_name, return_type=return_type)
 
 
-def fuse_data_area(df_base2area, df_area, df_input, amenity_fuse=None, amenity_brand_fuse=None, columns2rename=None, columns2drop=None, return_name = None, return_type=None):
-    # Cut data to given area 
+    # function to replace old fusion
+
+def fuse_data_area(df_base2area, df_area, df_input, amenity_fuse=None, amenity_set=False, amenity_brand_fuse=None, 
+                   columns2rename=None, columns2drop=None, column_set_value=None, columns2fuse=None, return_name = None, return_type=None):
+    # Cut input data to given area 
     df_input2area = gpd.overlay(df_input, df_area, how='intersection')
+
+    # Sort df by config settings from base dataframe
+    if amenity_fuse:
+        df_base_amenity = df_base2area[df_base2area.amenity == amenity_fuse]
+        df_base2area_rest = df_base2area[df_base2area.amenity != amenity_fuse]
+    elif amenity_brand_fuse:
+        amenity_brand_fuse = eval(amenity_brand_fuse)
+        df_base_amenity = df_base2area[((df_base2area.operator.str.lower() == amenity_brand_fuse[1].lower()) |
+                                        (df_base2area.brand.str.lower() == amenity_brand_fuse[1].lower())) & 
+                                        (df_base2area.amenity.str.lower() == amenity_brand_fuse[0].lower())]
+        df_base2area_rest = df_base2area[~df_base2area.apply(tuple,1).isin(df_base_amenity.apply(tuple,1))]
+    else:
+        print("Amenity (and brand) were not specified.. ")
+
+
+    if 'housenumber' not in df_input2area.columns:
+        for i in df_input2area.index:
+            df_row = df_input2area.iloc[i]
+            address = addr_deaggregate(df_row["addr:street"])
+            df_input2area.at[i,"addr:street"] = address[0]
+            df_input2area.at[i,"housenumber"] = address[1]
+
+    if column_set_value:
+        for col in column_set_value.keys():
+            df_input2area[col] = column_set_value[col]
+
     
-    return 
+
+    # Create temp sorted base dataframe 
+    df_base_temp = df_base_amenity[["geometry", "osm_id"]]      
+    # find closest points to fuse, default max distance for fusion 150 meters
+    # return 2 df - find closests and not
+    df_fus, df_not_fus = find_nearest(df_input2area, df_base_temp, 250)
+
+    fus_col_fus = columns2fuse
+    fus_col_fus.append("osm_id")
+    df_fus = df_fus[fus_col_fus]
+
+    # Prepare input data for concatination
+    if columns2rename:
+        df_not_fus = df_not_fus.rename(columns=columns2rename)
+    if columns2drop:
+        df_not_fus = df_not_fus.drop(columns={*columns2drop})
+
+    if amenity_brand_fuse:
+        df_not_fus['amenity'] = amenity_brand_fuse[0]
+        df_not_fus['operator'] = amenity_brand_fuse[1].lower()
+        columns2fuse.extend(('amenity', 'operator', 'source', 'geometry'))
+        df_not_fus = df_not_fus[columns2fuse]
+    elif amenity_set:
+        df_not_fus['amenity'] = amenity_fuse.lower()
+        columns2fuse.extend(('amenity', 'source', 'geometry'))
+        df_not_fus = df_not_fus[columns2fuse]
+    # df_not_fus['amenity'] = df_not_fus['amenity'].str.lower()
+
+    # Concatination of dataframes
+    df_result = (df_fus.set_index('osm_id').combine_first(df_base_amenity.set_index('osm_id')))
+    df_result = df_result.reset_index()
+    df_result = gpd.GeoDataFrame(df_result, geometry="geometry")
+    df_not_fus["osm_id"] = None 
+    df_result = pd.concat([df_result,df_not_fus],sort=False)
+    df_result = df_result.replace({np.nan: None})
+    df_result = pd.concat([df_result,df_base2area_rest],sort=False)
+    df_result.crs = "epsg:4326"
+
+    return gdf_conversion(df_result, return_name, return_type=return_type)
--- src/tests.py
+++ src/tests.py
@@ -10,153 +10,107 @@ from collection import Config, osm_collect_filter, gdf_conversion, bus_stop_conv
 from preparation import file2df, landuse_preparation, pois_preparation
 
 #from fusion import geonode_connection, fusion_data_areas_rs_set
-from fusion import replace_data_area, geonode_connection, geonode_table2df, area_n_buffer2df, base2area
-from credidentials_geonode import serv_password, db_password
+from fusion import replace_data_area, geonode_connection, geonode_table2df, area_n_buffer2df, df2area, df2geonode, fuse_data_area
+#from credidentials_geonode import serv_password, db_password
+from db.db import Database
 
 # POIS Collection + Preparation
 
-data_set = ["Mittelfranken","Niederbayern","Oberbayern","Oberfranken","Oberpfalz","Schwaben",\
-            "Unterfranken"]
+data_set = ["Mittelfranken","Niederbayern","Oberbayern","Oberfranken","Oberpfalz","Schwaben","Unterfranken"]
+
+config = Config("pois")
+config_buses = Config("bus_stops")
 
 ##===================================Collection POIs=============================================##
 
 # # 1st way - Create separate files for each region in list
 
-# config = Config("pois")
-# config_buses = Config("bus_stops")
-
 # for d in data_set:
-#     pois_collection = osm_collect_filter(config,d,update=False, driver="GPKG")
+#     pois_collection = osm_collect_filter(config,d,update=False)
 #     pois_bus_collection = join_osm_pois_n_busstops(pois_collection[0],bus_stop_conversion(osm_collect_filter(config_buses,d)[0]),pois_collection[1])
 
-#     pois_preparation(dataframe=pois_bus_collection[0], return_type="GPKG",result_name=pois_bus_collection[1])
+#     pois_preparation(dataframe=pois_bus_collection[0], config=config, return_type="GPKG",result_name=pois_bus_collection[1])
 
-# #=============##
+# #========================================================##
 
 # 2nd way - Download it separately and merge it together in one file k
 # Return type 'GPKG' could be used
+
+# data_name = "pois_bayern"
 
 # df_res = pd.DataFrame()
 
 # for d in data_set:
 #     pois_collection = osm_collect_filter(config,d, update=False)
-#     pois_bus_collection = join_osm_pois_n_busstops(pois_collection[0],bus_stop_conversion(osm_collect_filter(config_buses,d)[0]),pois_collection[1])
-#     temp_df = pois_preparation(dataframe=pois_bus_collection[0],result_name=pois_bus_collection[1])[0]
+#     pois_bus_collection = join_osm_pois_n_busstops(pois_collection[0],
+#                                                    bus_stop_conversion(osm_collect_filter(config_buses,d)[0]),
+#                                                    pois_collection[1])
+#     temp_df = pois_preparation(dataframe=pois_bus_collection[0], config=config, result_name=pois_bus_collection[1])[0]
 #     if data_set.index(d) == 0:
 #         df_res = temp_df
 #     else:
 #         df_res = pd.concat([df_res,temp_df],sort=False).reset_index(drop=True)
 
-# df = gdf_conversion(df_res, "pois_bayern", return_type='GPKG')[0]
+# df = gdf_conversion(df_res, data_name, return_type='GPKG')[0]
 
-# # GeoDataFrame to Geonode directly
-# engine = create_engine('postgresql://geonode_data:%s@161.97.133.234:5432/' % db_password )
-# df.to_postgis(con=engine, name="pois_bayern",if_exists='replace')
+# df = file2df("pois_bayern.gpkg")
 
+# df2geonode(df,data_name)
 
 #==============================================================Fusion POIs===========================================================================##
-# ====== Complete replace amenity ======
-# table_base = "pois_bayern"
+# ======  Fusion with config settings ======
 
-# rs_set = ["097610000", "095640000", "096630000"]
+con = geonode_connection()
 
-# table_input = "doctors_bavaria"
+table_base = config.fusion["table_base"]
+rs_set = config.fusion["rs_set"]
+typen = ["geonode","geojson"]
 
-# amenity_replace = "doctors"
-# columns2drop_input = ["id", "ref", "layer", "path"]
-# columns2rename = {"category": "amenity", "extras": "tags", "spider": "source"}
-# columns2drop_input =[]
+df_base = geonode_table2df(con, table_base, geometry_column="geom")
+df_area = area_n_buffer2df(con, rs_set, buffer=8300)
+df_base2area = df2area(df_base, df_area)
 
+for typ in typen:
+    fusion_key_set = config.fusion_key_set(typ)
 
-# return_name = "data_fused_newtest"
-# return_type = "GPKG"
+    for key in fusion_key_set:
+        print(key)
+        fusion_set = config.fusion_set(typ,key)
+        if typ == 'geojson':
+            filename = key + '.' + typ
+            df_input = file2df(filename)
+        elif typ == 'geonode':
+            df_input = geonode_table2df(con, key)
+        df_input2area = df2area(df_input, df_area)
+        
+        if config.fusion_type(typ, key) == "fuse":
+            df_base2area = fuse_data_area(df_base2area, df_area, df_input, *fusion_set, return_name = None, return_type=None)[0]
+        elif config.fusion_type(typ, key) == "replace":
+            df_base2area = replace_data_area(df_base2area, df_area, df_input, *fusion_set[:-1], return_name = None, return_type=None)[0]
+        else:
+            print("Fusion type for %s was not defined. Fusion was not done." % key)
+            pass
 
-
-# my_set = [amenity_replace,columns2drop_input,columns2rename,columns2drop_input, return_name, return_type]
-
-# print(my_set)
-
-# df_res = pd.DataFrame()
-
-# table_base = config.fusion["table_base"]
-# rs_set = config.fusion["rs_set"]
-# src_1 = "geonode"
-# src_2 = "geojson"
-# tables_input = config.fusion_key_set(src_1)
-# files_input = config.fusion_key_set(src_2)
-
-# iterat = 0
-# con = geonode_connection()
-
-# df_base = geonode_table2df(con, table_base,geometry_column="geom")
-# df_area = area_n_buffer2df(con,rs_set,buffer=8300)
-# df_base2area = base2area(df_base,df_area)
-
-# for t_input in tables_input:
-#     df_input = geonode_table2df(con, t_input)
-#     fusion_conf = config.fusion_set(src_1,t_input)
-#     df = replace_data_area(df_base2area, df_area, df_input, *fusion_conf)[0]
-#     if iterat == 0:
-#         df_res = df
-#     else:
-#         df_res = pd.concat([df_res,df],sort=False).reset_index(drop=True)
-
-# for f_input in files_input:
-#     filename = f_input + "." + src_2
-#     df_input = file2df(filename)
-#     fusion_conf = config.fusion_set(src_2,f_input)
-#     df = replace_data_area(df_base2area, df_area, df_input, *fusion_conf)[0]
-#     if iterat == 0:
-#         df_res = df
-#     else:
-#         df_res = pd.concat([df_res,df],sort=False).reset_index(drop=True)
-
-
-# df = gdf_conversion(df_res, "pois_study_area_merged", return_type='GPKG')[0]
-# =======
-# return_name = "data_fused_test"
-# return_type = "GeoJSON"
-
-# con = geonode_connection()
-# fusion_data_areas_rs_set(con, table_base, rs_set, table_input=table_input, amenity_replace=amenity_replace, columns2rename=columns2rename,
-#                                columns2drop_input=columns2drop_input, return_name=return_name, return_type=return_type)
-
-
-# ====== Complete replace brand ======
-# table_base = "pois_bayern"
-# file_input = "dm"
-# rs_set = ["097610000", "095640000", "096630000"]
-# amenity_replace = "doctors"
-# amenity_brand_replace = ("chemist","dm")
-# columns2drop_input = ["ref", "id"]
-# columns2rename = {"name": "brand", "@spider": "name"}
-
-
-# return_name = "data_fused_test_brand"
-# return_type = "GPKG"
-
-# con = geonode_connection()
-# fusion_data_areas_rs_set(con, table_base, rs_set, file_input=file_input, amenity_brand_replace=amenity_brand_replace, columns2rename=columns2rename,
-#                          columns2drop_input=columns2drop_input,return_name=return_name, return_type=return_type)
+gdf_conversion(df_base2area, "fusion_pois_test", return_type='GPKG')
 
 ##============================================================== Landuse Collection + Preparation + Export to GeoNode ===============================================================================##
 
-config = Config("landuse")
-df_res = pd.DataFrame()
+# config = Config("landuse")
+# df_res = pd.DataFrame()
 
-for d in data_set:
-    landuse_collection = osm_collect_filter(config,d, update=True)
+# for d in data_set:
+#     landuse_collection = osm_collect_filter(config,d, update=True)
 
-    temp_df = landuse_preparation(dataframe=landuse_collection[0],result_name=landuse_collection[1], config=config)[0]
-    if data_set.index(d) == 0:
-        df_res = temp_df
-    else:
-        df_res = pd.concat([df_res,temp_df],sort=False).reset_index(drop=True)
+#     temp_df = landuse_preparation(dataframe=landuse_collection[0],result_name=landuse_collection[1], config=config)[0]
+#     if data_set.index(d) == 0:
+#         df_res = temp_df
+#     else:
+#         df_res = pd.concat([df_res,temp_df],sort=False).reset_index(drop=True)
 
-df = gdf_conversion(df_res, "landuse_bayern", return_type=None)[0]
+# df = gdf_conversion(df_res, "landuse_bayern", return_type=None)[0]
 
 
-df.to_postgis(con=Database().connect_sqlalchemy(), name="landuse_bayern",if_exists='replace')
+# df.to_postgis(con=Database().connect_sqlalchemy(), name="landuse_bayern",if_exists='replace')
 
 ##==============================================================TESTS===============================================================================##
 # %%
